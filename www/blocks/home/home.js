9e8fa943c81e113b2a275d5e8a066961841e2844
Ben Lue <ben.lue@gocharm.com.tw>
2014-10-15 09:29:31 +0800

    add fb login

--- www/blocks/home/home.js
+++ www/blocks/home/home.js
@@ -1,4 +1,23 @@
 ctrl.onDisplayed = function()  {
+    // first check if the user has been login with a FB account
+    coim.sws.checkFB('public_profile', function(result) {
+        alert( JSON.stringify(result) );
+        if (result.errCode === 0)
+            _wf.loadPage( nextPage );
+        else
+            custLogin();
+    },
+    function(err) {
+        alert( JSON.stringify(err) );
+        custLogin();
+    });
+};
+
+/**
+ * Check if the user is registered with our own login scheme.
+ * If so, allow the user to proceed. Otherwise, redirect to the 'login' page.
+ */
+function  custLogin()  {
     coim.send('core/user/profile', {}, function(rtnData) {
         var  nextPage = 'login';
         if (rtnData.errCode === 0 && rtnData.value.isGuest != 1)
